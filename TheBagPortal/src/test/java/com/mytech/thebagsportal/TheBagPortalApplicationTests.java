e6bade5430f9598d3bf78b88ecaefc87fd3af4f7
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-05-11 14:56:05 +0700

    up

---
+++ TheBagPortal/src/test/java/com/mytech/thebagsportal/TheBagPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytech.thebagsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBagPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
